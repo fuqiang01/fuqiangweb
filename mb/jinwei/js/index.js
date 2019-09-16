c21f4657712e96409a34d42675f07a7deb494de1
fuqiang <206234924@qq.com>
2019-09-16 10:37:34 +0800

    仓储服务模板完成

--- mb/jinwei/js/index.js
+++
@@ -1,9 +0,0 @@
-
-
-// 新闻
-$('.news .last').on('click', function () {
-    $('#news-swiper').carousel('prev');
-})
-$('.news .next').on('click', function () {
-    $('#news-swiper').carousel('next');
-})
