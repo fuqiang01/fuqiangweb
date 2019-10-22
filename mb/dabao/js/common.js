fe2ae8cacf93bf34be34845f66f82a5210ed1cc4
fuqiang <206234924@qq.com>
2019-10-22 10:55:31 +0800

    新的简易模板完成

--- mb/dabao/js/common.js
+++ mb/dabao/js/common.js
@@ -1,5 +1,5 @@
 //侧边框
-$('.right-box li').hover(function() {
+$('.right-box li').hover(function () {
     $(this)
         .find('.hover-show')
         .slideDown();
@@ -8,7 +8,23 @@ $('.right-box li').hover(function() {
         .find('.hover-show')
         .slideUp();
 })
-$('.goTop').click( function () {
+$('.goTop').click(function () {
     console.log(111)
-    $('html,body').animate({'scrollTop': 0},800)
+    $('html,body').animate({ 'scrollTop': 0 }, 800)
+})
+
+// 弹窗
+$('.f-contactUs-btn').on('click', function () {
+    $('.popupWindow-wrapper').fadeIn();
+    return false;
+})
+$('.popupWindow-wrapper').on('click', function (e) {
+    if (e.target !== this) {
+        return;
+    }
+    $('.popupWindow-wrapper').fadeOut();
+    return false;
+})
+$('.popW-shut-btn').click(function () {
+    $('.popupWindow-wrapper').fadeOut();
 })
