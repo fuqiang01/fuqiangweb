24d0290ccf87e306e7dfab94b919eb0bd113d14f
fuqiang <206234924@qq.com>
2019-11-08 16:58:33 +0800

    111

---
+++ uni_app/hello-uniapp/pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="content">
+		<view class="item" @click="oneClick">
+			<image src="../../static/index_bg1.jpg" mode="widthFix"></image>
+			<text>科目一</text>
+		</view>
+		<view class="item">
+			<image src="../../static/index_bg2.jpg" mode="widthFix"></image>
+			<text>科目四</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			oneClick() {
+				uni.navigateTo({
+					url:'onepage'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+@import '../../common/style/index'
+</style>
