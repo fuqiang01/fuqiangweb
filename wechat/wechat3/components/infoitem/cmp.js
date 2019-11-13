f24d53c2e37928c12f42021e0438c7151f5edeca
fuqiang01 <206234924@qq.com>
2019-11-13 22:08:44 +0800

    小程序优化

--- wechat/wechat3/components/infoitem/cmp.js
+++ wechat/wechat3/components/infoitem/cmp.js
@@ -4,33 +4,79 @@ Component({
      * 组件的属性列表
      */
     properties: {
-
+        topicObj: Object
     },
 
     /**
      * 组件的初始数据
      */
     data: {
-        topicObj: {
-            id: 1,
-            titleType: "多选题",
-            titleText: "这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目",
-            options: [
-                "这是选项一这是选项一这是选项一这是选项一这是选项一",
-                "这是选项二",
-                "这是选项三",
-                "这是选项四"
-            ],
-            answer: [1,2],
-            explain: '详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释'
-        },
-        optionT: [ 'A', 'B', 'C', 'D', 'E', 'F', 'G']
+        // topicObj: {
+        //     id: 1,
+        //     titleType: "多选题",
+        //     titleText: "这是题目",
+        //     options: [
+        //         "这是选项一",
+        //         "这是选项二",
+        //         "这是选项三",
+        //         "这是选项四"
+        //     ],
+        //     answer: [1,2],
+        //     explain: '详细解释'
+        // },
+        optionT: [ 'A', 'B', 'C', 'D', 'E', 'F', 'G'],
+        classList: new Array(10),
+        showExplain: false
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
-
+        optionTap(e){
+            let index = e.currentTarget.dataset.index;
+            let arr = this.data.classList;
+            arr[index] = arr[index] == 'blue' ? '' : 'blue';
+            this.setData( {
+                classList: arr
+            })
+            if ( this.data.topicObj.titleType == "多选题" ) return;
+            if( index == this.data.topicObj.answer[0] ) {
+                this.triggerEvent('tonext');// 触发切换下一题事件
+            } else {
+                arr[index] = 'red';
+                this.setData({
+                    showExplain: true,
+                    classList: arr
+                })
+                console.log('将错题id上传服务器')
+            }
+        },
+        submitBtnTap(){
+            let classList = this.data.classList;
+            let showExplain = false;
+            for ( let i = 0; i < this.data.topicObj.options.length; i++ ) {
+                this.data.topicObj.answer.forEach(ele => {
+                    if ( classList[ele] != 'blue' ) {
+                        classList[ele] = 'green';
+                        showExplain = true;
+                    }
+                });
+                if ( classList[i] == 'blue' && !this.data.topicObj.answer.includes(i)){
+                    classList[i] = 'red';
+                    showExplain = true;
+                }
+            }
+            if ( !showExplain ){
+                this.triggerEvent('tonext'); // 触发切换下一题事件
+            } else {
+                this.setData({
+                    classList,
+                    showExplain
+                })
+                console.log('将错题id上传服务器')
+            }
+            
+        }
     }
 })
--- wechat/wechat3/pages/info/info.js
+++ wechat/wechat3/pages/info/info.js
@@ -5,14 +5,129 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        topicArr: [{
+                id: 1,
+                titleType: "多选题",
+                titleText: "1这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这这是题目这是题目这是题目这是题目这是题目这这是题目这是题目这是题目这是题目这是题目这是题目",
+                options: [
+                    "这是选项一这是选项一这是选项一这是选项一这是选项一",
+                    "这是选项二",
+                    "这是选项三",
+                    "这是选项四"
+                ],
+                answer: [1, 2],
+                explain: '详细解释详细解释详细解释详细解释详细解释详细解释详细详细解释详细解释详细解释详细解释详细解释详细解释详细详细解释详细解释详细解释详细解释详细解释详细解释详细详细解释详细解释详细解释详细解释详细解释详细解释详细详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释1'
+            },
+            {
+                id: 2,
+                titleType: "多选题",
+                titleText: "2这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目",
+                options: [
+                    "这是选项一这是选项一这是选项一这是选项一这是选项一",
+                    "这是选项二",
+                    "这是选项三",
+                    "这是选项四"
+                ],
+                answer: [1, 2],
+                explain: '1详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释'
+            },
+            {
+                id: 3,
+                titleType: "多选题",
+                titleText: "3这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目",
+                options: [
+                    "这是选项一这是选项一这是选项一这是选项一这是选项一",
+                    "这是选项二",
+                    "这是选项三",
+                    "这是选项四"
+                ],
+                answer: [1, 2],
+                explain: '1详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释'
+            },
+            {
+                id: 4,
+                titleType: "多选题",
+                titleText: "4这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目",
+                options: [
+                    "这是选项一这是选项一这是选项一这是选项一这是选项一",
+                    "这是选项二",
+                    "这是选项三",
+                    "这是选项四"
+                ],
+                answer: [1, 2],
+                explain: '1详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释'
+            },
+            {
+                id: 5,
+                titleType: "多选题",
+                titleText: "5这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目这是题目",
+                options: [
+                    "这是选项一这是选项一这是选项一这是选项一这是选项一",
+                    "这是选项二",
+                    "这是选项三",
+                    "这是选项四"
+                ],
+                answer: [1, 2],
+                explain: '1详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释详细解释'
+            }
+        ],
+        current: 0,
+        showIndex: 0,
+        isTouch: false
     },
-
+    toNext() {
+        console.log('收到跳转下一页信号')
+        if (this.data.current >= this.data.topicArr.length) return;
+        this.setData({
+            current: this.data.current + 1
+        })
+    },
+    onStart() {
+        this.setData({
+            isTouch: true
+        })
+    },
+    onFinish(e) {
+        if (e.detail.source != 'touch') {
+            this.setData({
+                isTouch: false
+            })
+            return;
+        }
+        let current = e.detail.current;
+        let showIndex = this.data.showIndex;
+        if (current > 0) {
+            showIndex = showIndex >= this.data.topicArr.length - 1 ? this.data.topicArr.length - 1 : showIndex + 1;
+        } else {
+            showIndex = showIndex <= 0 ? 0 : showIndex - 1;
+        }
+        current = showIndex == 0 ? 0 : 1;
+        this.setData({
+            showIndex,
+            current
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        console.log(options)
+        console.log(options);
+        let self = this;
+        wx.request({
+            url: 'https://www.fqiang.co/getTopicAll',
+            data: {
+                type: options.num
+            },
+            method: 'GET',
+            success(res) {
+                self.setData({
+                    topicArr: res.data
+                })
+            },
+            fail(err) {
+                console.log(err);
+            }
+        })
     },
 
     /**
