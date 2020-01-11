7a3a2352144a83c62bb36436988151a33a449389
fuqiang <206234924@qq.com>
2020-01-11 17:04:54 +0800

    cs

--- vue/biog-bg/src/components/addArticle/RichText.vue
+++ vue/biog-bg/src/components/addArticle/RichText.vue
@@ -28,7 +28,7 @@ export default {
                         loading: true,
                         name: "img",
                         action:
-                            "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+                            "http://127.0.0.1:12306/getImg",
                         response: res => {
                             return res.url;
                         }
