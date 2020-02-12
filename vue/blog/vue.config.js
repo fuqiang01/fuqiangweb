f5f5458ecb39cb09b092525706137f9efe141ed6
fuqiang01 <206234924@qq.com>
2020-02-13 01:05:15 +0800

    添加cdn加速测试

--- vue/blog/vue.config.js
+++ vue/blog/vue.config.js
@@ -2,19 +2,36 @@ const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
     productionSourceMap: false, // 不生成.map文件
-    configureWebpack: () => {
-        if(process.env.NODE_ENV === 'production'){
-            return {
-                plugins: [
-                    new CompressionPlugin({
-                        test: /\.js$|\.css$/,
-                        threshold: 10240,
-                        deleteOriginalAssets: false
-                    })
-                ]
-            }
+    // configureWebpack: () => {
+    //     if (process.env.NODE_ENV === 'production') {
+    //         return {
+    //             plugins: [
+    //                 new CompressionPlugin({
+    //                     test: /\.js$|\.css$/,
+    //                     threshold: 10240,
+    //                     deleteOriginalAssets: false
+    //                 })
+    //             ]
+    //         }
+    //     }
+    // },
+    configureWebpack: {
+        plugins: [
+            new CompressionPlugin({
+                test: /\.js$|\.css$/,
+                threshold: 1024,
+                deleteOriginalAssets: false
+            })
+        ],
+        externals: {
+            'vue': 'Vue',
+            'vuex': 'Vuex',
+            'vue-router': 'VueRouter',
+            'ant-design-vue': 'antd',
+            'axios': 'axios'
         }
     },
+
     // devServer: {
     //     proxy: {
     //         "/*": {
