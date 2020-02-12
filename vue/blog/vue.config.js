4802251b60a35581b4ec3cbfeb7dff151afbcc0a
fuqiang01 <206234924@qq.com>
2020-02-12 23:06:48 +0800

    使用gzip压缩测试

--- vue/blog/vue.config.js
+++ vue/blog/vue.config.js
@@ -1,4 +1,20 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
+    productionSourceMap: false, // 不生成.map文件
+    configureWebpack: () => {
+        if(process.env.NODE_ENV === 'production'){
+            return {
+                plugins: [
+                    new CompressionPlugin({
+                        test: /\.js$|\.css$/,
+                        threshold: 10240,
+                        deleteOriginalAssets: false
+                    })
+                ]
+            }
+        }
+    },
     // devServer: {
     //     proxy: {
     //         "/*": {
