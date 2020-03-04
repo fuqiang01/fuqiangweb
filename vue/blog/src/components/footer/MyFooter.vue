7e423c1172120cbeda84eb097ff62b992569d0b6
fuqiang01 <206234924@qq.com>
2020-03-04 11:16:30 +0800

    修改fqla的备案号

--- vue/blog/src/components/footer/MyFooter.vue
+++ vue/blog/src/components/footer/MyFooter.vue
@@ -2,7 +2,7 @@
     <footer>
         <div>
             备案号：
-            <a href="http://www.beian.miit.gov.cn/" target="_blank">鄂ICP备19011265号-2</a>
+            <a href="http://www.beian.miit.gov.cn/" target="_blank">鄂ICP备19011265号-3</a>
             | 有任何建议或者问题欢迎发送邮件到：206234924@qq.com
         </div>
     </footer>
