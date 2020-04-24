46a8bb5f2117be7fe247a8872d224ecbd957b56f
fuqiang <206234924@qq.com>
2020-04-24 17:31:39 +0800

    新的爬虫

---
+++ node/crawler_jz/crawler.my.js
@@ -0,0 +1,60 @@
+const axios = require("axios");
+const fs = require("fs");
+
+
+function uploadTopic(topicData) {
+    return axios.post("http://localhost:3000/topic/create", topicData)
+}
+
+async function downloadImg(imgSrc, num) {
+    const result = await axios({
+        url: `https://www.fqiang.co/getImg?src=${imgSrc}`,
+        responseType: 'stream'
+    })
+    const writer = fs.createWriteStream(`./img/${num}.jpg`)
+    result.data.pipe(writer)
+    return `${num}.jpg`;
+}
+
+
+async function root() {
+    const result = await axios("https://www.fqiang.co/getTopicAll?type=4");
+    for (let i = 0; i < result.data.length; i++) {
+        const ele = result.data[i];
+        let imgUrl = "";
+        let testType = "";
+        if (ele.imgSrc) {
+            imgUrl = await downloadImg(ele.imgSrc, i + 1)
+        }
+        switch (ele.titleType) {
+            case "单选题":
+                testType = 'radio';
+                break;
+            case "多选题":
+                testType = "check";
+                break;
+            case "判断题":
+                testType = "judge";
+        }
+        const topicData = {
+            subject: 1,
+            title: ele.titleText,
+            answer: ele.answer,
+            topicExplain: ele.explain,
+            imgUrl,
+            options: ele.options,
+            testType,
+            lnowledgeType: "mark"
+        }
+        await uploadTopic(topicData)
+            .then(res => {
+                console.log(`第${i + 1}题，上传${res.data.msg}!`);
+                if (res.data.msg === "失败") console.log("错误信息：", res.data.data);
+            }, err => {
+                console.log("上传出错，错误信息：", err)
+            })
+    }
+
+}
+
+root();
--- node/crawler_jz/index.js
+++ node/crawler_jz/index.js
@@ -1,64 +1,79 @@
-<<<<<<< HEAD
-const Axios = require("axios");
-const Cheerio = require("cheerio");
-const IconvLite = require("iconv-lite");
-
-
-Axios.get("http://www.jsyst.cn/mtc/km1/fx/q201/")
-    .then(res => {
-        console.log(Object.keys(res))
-        // console.log(res.data)
-        const $ = Cheerio.load(res.data);
-        const text = $(".common_bor h1 a").text();
-        console.log(text)
-        const a = IconvLite.decode(text, "utf-8");
-        console.log(a)
-    })
-=======
 const cheerio = require("cheerio");
 const axios = require('axios');
 const iconv = require('iconv-lite');
-const fs = require("fs");
 const {
     downloadImg
 } = require("./util");
 
-const url = 'http://km1.jsyst.cn/fx/q201/';
-function crawl(url) {
-    return axios({
-            url,
-            responseType: 'stream' //将数据转化为流返回
-        })
-        .then(res => new Promise((resolve, reject) => {
-            let chunks = [];
-            res.data.on('data', chunk => {
-                chunks.push(chunk);
-            });
-            res.data.on('end', () => {
-                const buffer = Buffer.concat(chunks);
-                //通过iconv来进行转化。
-                const html = iconv.decode(buffer, 'gbk');
-                const $ = cheerio.load(html);
-                const title = $('.vehiclesIn3 h1').text();
-                const answer = $('.vehiclesIn3 p font').text();
-                const comment = $('.vehiclesIn3 hr').prev("p")[0].children[1].data;
-                let options = [];
-                $('.vehiclesIn3 p').each((index, ele) => {
-                    if ($(ele)[0].children[0] !== undefined && $(ele)[0].children[0].type === "text") {
-                        const text = $(ele)[0].children[0].data;
-                        options.push(text);
-                    }
-                })
-                const imgSrc = $('.vehiclesIn3 p img').attr("src");
-                const fileName = imgSrc ? downloadImg(imgSrc) : null;
-                resolve({
-                    title,
-                    answer,
-                    comment,
-                    img: fileName,
-                    options
-                })
+async function crawl(url) {
+
+    const res = await axios({
+        url,
+        responseType: 'stream' //将数据转化为流返回
+    })
+
+    return new Promise(resolve => {
+        let chunks = [];
+        res.data.on('data', chunk => {
+            chunks.push(chunk);
+        });
+        res.data.on('end', () => {
+            const buffer = Buffer.concat(chunks);
+            //通过iconv来进行转化。
+            const html = iconv.decode(buffer, 'gbk');
+            const $ = cheerio.load(html);
+
+            const title = $('.vehiclesIn3 h1').text();
+            const comment = $('.vehiclesIn3 hr').prev("p")[0].children[1].data;
+            const answer = $('.vehiclesIn3 p font').text().replace("A", 1).replace("B", 2).replace("C", 3).replace("D", 4);
+
+            let options = [];
+            $('.vehiclesIn3 p').each((index, ele) => {
+                if ($(ele)[0].children[0] !== undefined && $(ele)[0].children[0].type === "text") {
+                    const text = $(ele)[0].children[0].data.slice(2);
+                    options.push(text);
+                }
             })
-        }))
+            const imgSrc = $('.vehiclesIn3 p img').attr("src");
+            const fileName = imgSrc ? downloadImg(imgSrc) : null;
+            let testType = options.length === 2 ? "judge" : "radio";
+            resolve({
+                subject: 1,
+                title,
+                answer: [answer],
+                topicExplain: comment,
+                imgUrl: fileName,
+                options,
+                testType,
+                lnowledgeType: "mark"
+            })
+        })
+    })
 }
->>>>>>> d13618b381a32ae5f8dfaeb66619f8eda31d64de
+
+function uploadTopic(topicData) {
+    return axios.post("http://localhost:3000/topic/create", topicData)
+}
+
+let num = 1;
+let url = `http://www.jsyst.cn/mtc/km1/fx/q${num}/`;
+
+function root(url) {
+    crawl(url)
+        .then(res => {
+            return uploadTopic(res)
+        }, err => {
+            console.log("页面出错，url地址：", url)
+        })
+        .then(res => {
+            console.log(`第${num}题，上传${res.data.msg}!`);
+            if(res.data.msg === "失败") console.log("错误信息：", res.data.data);
+            num ++;
+            const newUrl = `http://www.jsyst.cn/mtc/km1/fx/q${num}/`
+            root(newUrl);
+        }, err => {
+            console.log("上传出错，错误信息：", err)
+        })
+}
+
+root(url);
