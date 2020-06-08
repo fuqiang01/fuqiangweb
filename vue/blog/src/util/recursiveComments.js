7d983b203900649c62eff5e84bca6170c7605713
fuqiang01 <206234924@qq.com>
2020-06-08 19:47:50 +0800

    删除已近单独提出来了的项目

--- vue/blog/src/util/recursiveComments.js
+++
@@ -1,8 +0,0 @@
-export default function recursiveComments(data, callBack){
-        data.forEach(ele => {
-            callBack(ele);
-            if (ele.children && ele.children.length > 0) {
-                recursiveComments(ele.children, callBack);
-            }
-        });
-}
